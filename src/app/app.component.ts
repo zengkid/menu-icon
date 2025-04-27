dcc5a6b999bee8ae86ee4be75245e40dab069a21
zengkid <zengkid@msn.com>
2025-04-27 20:07:12 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,86 @@
+import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzMenuItemComponent } from 'ng-zorro-antd/menu';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
+  imports: [NgTemplateOutlet,RouterOutlet, NzIconModule, NzLayoutModule,
+    NzMenuModule, NzSpaceModule,NzMenuItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   isCollapsed = false;
+
+  menus = [
+    {
+      level: 1,
+      label: '系统管理',
+      icon: 'icons:system',
+      open: true,
+      selected: false,
+      children: [
+        {
+          level: 2,
+          label: '用户管理',
+          icon: 'icons:manage_accounts',
+          open: false,
+          selected: false,
+          children: [
+            {
+              level: 3,
+              icon: 'icons:manage_accounts',
+              label: 'Option 1',
+              selected: false,
+            },
+            {
+              level: 3,
+              label: 'Option 2',
+              icon: 'icons:manage_accounts',
+              selected: false,
+            }
+          ]
+        },
+        {
+          level: 2,
+          label: 'Group 2',
+          icon: 'icons:manage_accounts',
+          selected: true,
+        },
+        {
+          level: 2,
+          label: 'Group 3',
+          icon: 'icons:systemapp',
+          selected: false,
+        }
+      ]
+    },
+    {
+      level: 1,
+      label: 'Team Group',
+      icon: 'team',
+      open: false,
+      selected: false,
+      children: [
+        {
+          level: 2,
+          label: 'User 1',
+          icon: 'user',
+          selected: false,
+        },
+        {
+          level: 2,
+          label: 'User 2',
+          icon: 'user',
+          selected: false,
+        }
+      ]
+    }
+  ];
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,17 +2,25 @@ import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } fr
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { icons } from './icons-provider';
 import { provideNzIcons } from 'ng-zorro-antd/icon';
-import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { zh_CN, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
+import zh from '@angular/common/locales/zh';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import * as AllIcons from '@ant-design/icons-angular/icons';
+import { IconDefinition } from '@ant-design/icons-angular';
 
-registerLocaleData(en);
+registerLocaleData(zh);
+
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(
+  (key) => antDesignIcons[key]
+);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideNzIcons(icons), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideNzIcons(icons), provideNzI18n(zh_CN), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()]
 };
